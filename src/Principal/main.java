a8c3ce737391b984aa488c2cfa84c49ca0454207
@Norione <>
2023-05-11 21:01:43 -0500

    Class Principal Main

---
+++ src/Principal/main.java
@@ -0,0 +1,22 @@
+package Principal;
+
+
+import Ventanas.VentanaPrincipal;
+
+/**
+ *
+ * @author ivan
+ */
+
+public class main {
+    public static String nombre_1;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+        ventanaPrincipal.setLocationRelativeTo(null);
+    }
+    
+}
